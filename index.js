92855eab2c35b09918254fe35345c532c2eac34b
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-03-12 18:33:24 +0530

    Minutes hand applied

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const secondHand = document.querySelector`.second-hand`
+const minsHand = document.querySelector`.min-hand`
+
 
 
 function setDate(){
   const  now = new Date();
 
-  const seconds = now.getSeconds;
+  const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60)* 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((minutes / 60)* 360) + 90;
+  minsHand.style.transform = `rotate(${minutesdegrees}deg)`;
   
 }
 
